0f7907f1ed83b6262882e8878ebffa0017be41e6
코딩 수련생_수나 <sh227sim@gmail.com>
2024-07-18 20:35:35 +0900

    [Silver V] Title: 덩치, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,17 +1,17 @@
-N = int(input())
-people = []
-rates = []
+N = int(input()) # 인원수
+people = [] # 인원 정보 넣을 리스트
+rates = [] # 덩치 등수 넣을 리스트
 
-for _ in range(N):
-    W, H = map(int, input().split())
-    people.append([W, H])
+for _ in range(N): # 인원수 만큼 for문 반복
+    W, H = map(int, input().split()) # 몸무게, 키 정보 입력
+    people.append([W, H]) # 인원 정보 리스트에 추가
 
-for i in range(N):
-    rate = 1
+for i in range(N): # 인원수들 덩치 비교
+    rate = 1 # 덩치 등수 기본값 1로 설정
     for j in range(N):
         if people[i][0] < people[j][0] and people[i][1] < people[j][1]:
-            rate += 1
-    rates.append(rate)
+            rate += 1 # 나보다 큰 사람이 있으면 + 1
+    rates.append(rate) # 덩치 등수 리스트에 추가
 
 for i in rates:
     print(i, end=" ")
