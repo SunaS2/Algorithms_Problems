f8492a655721a5d2da5b7c5cfdbf8e0a81d61dc4
shSim <sh227sim@gmail.com>
2024-07-31 00:52:46 +0900

    20240730 수업시간 문제 풀이완료

--- SWEA/algorithm/list/12467-SWEA.py
+++ SWEA/algorithm/list/12467-SWEA.py
@@ -7,29 +7,39 @@ for tc in range(1, T+1):
 
     for charger in chargers:
         stops[charger] = 1
+
+    
     charging = 0
     present_position = 0
-    while present_position >= len(stops):
-        if stops[present_position+k] == 1:
+    cannotgo = 0
+
+    while present_position+k < m:
+        previous_charging = present_position
+        present_position += k
+        if stops[present_position] == 1:
             charging += 1
             previous_charging = present_position
+            continue
         else:
-            for i in range(1,k):
-                if stops[present_position-i] == 1:
-                    charging += 1
-                    present_position = present_position-i
-                    break
+            count_charger = 0
+            idx = 0
+            for i in range(1,len(stops[previous_charging:present_position])):
+                if stops[previous_charging+i] == 1:
+                    count_charger += 1
+                    idx = i
+            if count_charger >= 1:
+                present_position = previous_charging + idx
+                charging += 1
+            elif count_charger == 0:
+                cannotgo = 1
+        
+    if cannotgo == 1:
+        print(f'#{tc} {cannotgo-1}')
+    else:
+        print(f'#{tc} {charging}')
+
 #순화언니 안뇽!!!!!!!!!!!!!
 #내가 누구게
-        new_charging = charging
-        if charging == new_charging:
-            charging = 0
-            break
-
-        previous_charging = present_position
-
-    print(f'#{tc} {charging}')
-
     # while idx <= len(stops):
     #     for i in range(rest_chargers,0,-1):
     #         if (stops[chargers[charger_idx + i]] - previous_charger) > k:
@@ -64,9 +74,3 @@ for tc in range(1, T+1):
 
     # if cannotgo == 1:
     #     count_charging = 0
-
-
-
-
-
-
