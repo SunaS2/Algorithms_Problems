4264122e0b4e1b7602e089431a536c8976bb4f8b
코딩 수련생_수나 <sh227sim@gmail.com>
2024-08-15 21:26:27 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,21 @@
+def backtrack(row, m):
+    global now
+    if row == m:
+        print(*result)
+        return
+
+    for i in range(len(lst)):
+        if check[i] == False and now < lst[i]:
+            check[i] = True #방문했어요 방명록 작성
+            result[row] = lst[i] #이번 스텝의 결과 리스트에 고른 순열 담기
+            backtrack(row+1,m) #다음 스텝으로 넘어가기
+            check[i] = False #다음 스텝에 갔다가 돌아오면 나가기 체크아웃
+            now = result[row]
+            result[row] = 0 #체크아웃 하면서 결과 리스트도 원상복구 시키기
+
+n, m = map(int,input().split())
+check = [False] * n
+lst = [x for x in range(1,n+1)]
+result = [0] * m
+now = result[0]
+backtrack(0,m)
