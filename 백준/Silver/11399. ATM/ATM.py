6cb5fa8c06e6fcd3d3d49575bcfafba75339ef76
코딩 수련생_수나 <sh227sim@gmail.com>
2024-09-12 15:46:41 +0900

    [Silver IV] Title: ATM, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,23 +1,8 @@
-def selectionsort(lst,n):
-    for i in range(n):
-        min_idx = i
-        for j in range(i+1,n):
-
-
-            if lst[j] < lst[min_idx]:
-                min_idx = j
-        lst[i], lst[min_idx] = lst[min_idx], lst[i]
-
-    return lst
-
 N = int(input())
-people = list(map(int, input().split()))
+times = sorted(map(int, input().split()))
+ans = 0
 
-sorted_people = selectionsort(people,N)
+for i in range(N+1):
+    ans += sum(times[:i])
 
-result = 0
-for i in range(N):
-    wait=sum(sorted_people[:i+1])
-    result+=wait
-
-print(result)
+print(ans)
