4fedaa37f1c0bcf810f39b4ac95769a7a746c301
코딩 수련생_수나 <sh227sim@gmail.com>
2024-09-12 15:49:24 +0900

    [Silver IV] Title: ATM, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,8 +1,9 @@
-N = int(input())
-times = sorted(map(int, input().split()))
+N = int(input()) # 사람 수 N
+times = sorted(map(int, input().split())) # 걸리는 시간
+# 필요한 시간의 합의 최솟값을 구해야한다 => 오름차순으로 정렬
 ans = 0
 
-for i in range(N+1):
-    ans += sum(times[:i])
+for i in range(1, N+1): # 1번부터 N번까지
+    ans += sum(times[:i]) # i번 사람까지 걸리는 시간의 합을 누적
 
 print(ans)
