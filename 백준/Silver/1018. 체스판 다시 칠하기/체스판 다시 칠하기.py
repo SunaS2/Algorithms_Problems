0b9c02329e07ddcc29f2086e2814ca5cdd0fcf5b
코딩 수련생_수나 <sh227sim@gmail.com>
2024-09-14 17:22:53 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 116 ms, Memory: 111260 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,28 +1,29 @@
-N, M = map(int, input().split())
 
-chess = [input() for _ in range(N)] # 내게 주어진 체스판
-ans = 1e10 # 최소값 설정 임의값
+def count_recoloring(start_r,start_c):
+    W_cnt = 0
+    B_cnt = 0
+    for r in range(start_r,start_r+8):
+        for c in range(start_c,start_c+8):
+            if (r+c) % 2 == 0:
+                if board[r][c] != 'B':
+                    B_cnt += 1
+                if board[r][c] != 'W':
+                    W_cnt += 1
+            else:
+                if board[r][c] != 'B':
+                    W_cnt += 1
+                if board[r][c] != 'W':
+                    B_cnt += 1
 
-for i in range(N-7): # 8 * 8 체스판으로 잘라낸 후 확인
-    for j in range(M-7): # 체스판의 시작점 for문
-        white_recover = 0 # 흰색으로 새로 색칠한 갯수
-        black_recover = 0 # 검정색으로 새로 색칠한 갯수
+    result.append(W_cnt)
+    result.append(B_cnt)
 
-        for a in range(i, i+8): # 잘라낸 부분의 시작점에서 8*8 만큼 for문 실행
-            for b in range(j, j+8):
-                if (a+b) % 2 == 0: # 짝수칸 ( == 체스판의 [0][0]과 같은 색깔 )
-                    if chess[a][b] != "W":
-                        white_recover += 1
-                    else:
-                        black_recover += 1
-                else: # 홀수칸
-                    if chess[a][b] != "B":
-                        white_recover += 1
-                    else:
-                        black_recover += 1
-        
-        recover = min(white_recover, black_recover)
-        if ans > recover:
-            ans = recover
+N, M = map(int,input().split())
+board = [list(input()) for _ in range(N)]
 
-print(ans)
+result= []
+for r in range(N-7):
+    for c in range(M-7):
+        count_recoloring(r,c)
+
+print(min(result))
